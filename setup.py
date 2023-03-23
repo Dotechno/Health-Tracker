2e6839e28709e12c2fc206cf873822c518cb59a5
Matthew Z <61751438+Mattz-CE@users.noreply.github.com>
2023-03-22 18:22:53 -0700

    Update setup.py
    Hot fix setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-# setup DB
-from run import db, app
+import sys
+from flask import Flask
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'arbitrarySecretKey'
+
+
+if not 'models' in sys.modules:
+    from model import db
 
 with app.app_context():
     db.create_all()
