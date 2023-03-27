d8f9189213714da9b358ea771607411ae0fe75fc
Matt <61751438+Mattz-CE@users.noreply.github.com>
2023-03-26 18:57:31 -0700

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from datetime import datetime
 # Initialize app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SECRET_KEY'] = 'arbitrarySecretKey'
+app.config['SECRET_KEY'] = '2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6'
 
 # db = SQLAlchemy(app)
 
