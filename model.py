504b4d167d28099df9a1458c163e3ff04a2f2861
Matt <61751438+Mattz-CE@users.noreply.github.com>
2023-04-16 19:03:19 -0700

    Update model.py

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
+from enum import Enum
+
 db = SQLAlchemy(app)
 
 
